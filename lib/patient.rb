fef9e2dfb73a4eaa547b44d2018eec45187bf080
Uriah Harston <uharston@gmail.com>
2020-05-20 17:31:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,8 +9,8 @@ class Patient
     @@all << self
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
   end
 
   def appointment
@@ -24,4 +24,4 @@ class Patient
   def self.all
     @@all
   end
-end 
+end
