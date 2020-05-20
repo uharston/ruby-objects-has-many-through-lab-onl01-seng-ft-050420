0011f63a564c0dfd2b0d7c63cc9e5730a7b2bfdf
Uriah Harston <uharston@gmail.com>
2020-05-20 17:28:46 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+
+class Doctor 
+  attr_reader :name 
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def new_appointment(patient, appointment)
+    Appointment.new(patient, self, appointment)
+  end 
+
+  def appointments 
+    Appointment.all.select {|a| a.doctor == self}
+  end 
+
+  def patients 
+    appointment.map {|a| a.patient}
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+
+end 
