654317fa6461d8dfe3fdc5907c272b95190bd3df
Uriah Harston <uharston@gmail.com>
2020-05-20 17:15:31 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,4 +3,17 @@ class Genre
   attr_reader
 
   @@all = []
+
+  def initialize(genre)
+    @genre = genre 
+    @@all << self 
+  end 
+
+  def songs 
+    Song.all.select {|s| s.genre == self}
+  end 
+
+  def self.all 
+    @@all 
+  end 
 end
